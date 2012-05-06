4ec21bc13845c682dfc6d8369cc06bc175d0120b
Fernando Alonso <krakatoa1987@gmail.com>
2012-05-06 01:29:27 -0300

    Using kb collections

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ var Person = Backbone.Model.extend({
   //}
 });
 
-//var People = Backbone.Collection.extend({
-//  model: Person
-//});
+var People = Backbone.Collection.extend({
+  model: Person,
+  url: '/people'
+});
 
 //var PersonViewModel = function(model) {
 //  this.name = model.get('name');
@@ -41,10 +42,35 @@ PersonViewModel = kb.ViewModel.extend({
   }
 });
 
-var person = new Person();
+//var person = new Person();
 
-var person_view_model = new PersonViewModel(person);
+//var person_view_model = new PersonViewModel(person); // solamente es necesario instanciarlo si se lo va a bindear
 
-ko.applyBindings(person_view_model);
+var people_collection = new People();
+people_collection.add(//[
+  //{name: 'Pepe', lastName: 'Iglesias'},
+  //{name: 'Ronaldinho', lastName: 'Gaullo'}
+//]
+  people_array
+);
+
+//Backbone.sync = function(method, model, options) {
+//  alert(method + ": " + model);
+//  options.error(model);
+//};
+
+var PeopleViewModel = kb.ViewModel.extend({
+  constructor: function(collection) {
+    this.people = ko.observableArray([]);
+    kb.collectionObservable(collection, this.people, {
+      view_model: PersonViewModel
+    });
+    return this;
+  }
+});
+
+people_view_model = new PeopleViewModel(people_collection); // se lo instancia porq es el q se bindea
+
+ko.applyBindings(people_view_model);
 
 //kb.vmRelease(person_view_model);
